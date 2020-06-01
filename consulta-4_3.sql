dee901e0ca04ede1726c180d20ab9d4bd5a06422
Wander Douglas <wanderdasouza@gmail.com>
2020-06-01 16:26:07 -0300

    Adiciona script para a consulta 4.4

--- consulta-4_3.sql
+++ consulta-4_3.sql
@@ -1,4 +1,10 @@
-SELECT nome,classe
+SELECT DISTINCT servico.nome,servico.classe
 FROM servico
-INNER JOIN tutelamento
-      ON servico.id_servico=tutelamento.id_servico
+INNER JOIN pertence
+      ON servico.id_servico=pertence.id_servico
+INNER JOIN perfil
+      ON pertence.id_perfil=perfil.id_perfil
+INNER JOIN possui
+      ON perfil.id_perfil = possui.id_perfil
+INNER JOIN usuario
+      ON possui.id_usuario = usuario.id_usuario
