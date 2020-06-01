d2228d9fa319af5e20aee3dc78eb2642fc67e247
Rodrigo Orem <orem.rodrigo@gmail.com>
2020-06-01 17:14:39 -0300

    Move all sql to sql/

---
+++ sql/insert-amostra.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.amostra (
+  id_paciente, id_exame, codigo_amostra, metodo_de_coleta, material
+)
+VALUES
+  (1, 1, 1, 'Raspagem de nasofaringe', 'Secreção de nasofaringe'),
+  (2, 2, 2, 'Coleta de sangue', 'Sangue'),
+  (3, 3, 3, 'Raspagem no pênis', 'Secreção do pênis'),
+  (6, 4, 4, 'Raspagem no colo do útero', 'Secreção do colo do útero'),
+  (8, 5, 5, 'Raspagem da orofaringe', 'Secreção de orofaringe'),
+  (8, 5, 6, 'Coleta de sangue', 'Sangue'),
+  (4, 1, 7, 'Raspagem da nasofaringe', 'Secreção de nasofaringe'),
+  (5, 4, 8, 'Raspagem no colo do útero', 'Secreção do colo do útero'),
+  (7, 3, 9, 'Raspagem no pênis', 'Secreção do pênis'),
+  (7, 2, 10, 'Coleta de sangue', 'Sangue');
