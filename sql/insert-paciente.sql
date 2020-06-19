8ebc7dbb04a2ac49648023c2094bddc4c19075e8
Wander Souza <wanderdasouza@gmail.com>
2020-06-19 19:03:15 -0300

    Cria script para adicionar pacientes como pessoas

--- sql/insert-paciente.sql
+++ sql/insert-paciente.sql
@@ -1,73 +1,43 @@
 INSERT INTO paciente(
-    id_paciente, cpf, nome, endereco, nascimento)
+    id_paciente, cpf)
 VALUES
 (
     1,
     '70474350185',
-    'Denner Oliveira',
-    'Rodovia Sávio, 337 - Vila Weseley, Tocantins',
-    '2000-09-02T21:28:22.040Z'
 ),
 (
     2,
     '67912886710',
-    'Jürgen Reis',
-    'Viela Laís, 356 - Grande Evely, Mato Grosso',
-    '2003-09-06T01:33:28.759Z'
 ),
 (
     3,
     '38930836500',
-    'Tomiko Braga',
-    'Avenida Lilian, 976 - Grande Celso, Pernambuco',
-    '1964-06-05T04:02:25.200Z'
 ),
 (
     4,
     '83681837236',
-    'Jevetic Saraiva',
-    'Rua Elaine, 721 - Velha Susana, Amazonas',
-    '1953-04-21T16:34:20.488Z'
 ),
 (
     5,
     '40888079443',
-    'Cristiani Braga',
-    'Avenida Sílvio, 790 - Vila Zuleno, Alagoas',
-    '1958-03-19T22:52:20.283Z'
 ),
 (
     6,
     '72531952306',
-    'Yulle Carvalho',
-    'Travessa Toshio, 436 - Município de Analeia, Sergipe',
-    '1948-09-01T04:40:05.948Z'
 ),
 (
     7,
     '96553777500',
-    'Ilson Macedo',
-    'Ponte Fabrisa, 336 - Vila Elma, São Paulo',
-    '1964-06-22T04:01:09.038Z'
 ),
 (
     8,
     '61960354876',
-    'Joelmir Pereira',
-    'Alameda Giovani, 641 - Município de Osmundo, Acre',
-    '1953-12-03T15:48:22.079Z'
 ),
 (
     9,
     '63122761718',
-    'Maricy Oliveira',
-    'Rodovia Christine, 106 - Município de Ereciano, Maranhão',
-    '1960-02-16T20:19:08.064Z'
 ),
 (
     10,
     '70074874322',
-    'Toshio Carvalho',
-    'Rua Gessé, 992 - Vila Wladimir, Piauí',
-    '1993-06-19T15:24:21.486Z'
 );
