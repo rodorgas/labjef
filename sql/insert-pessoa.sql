bacfca1e4a6296f1ac5c95e6ff4da1e33bbf2648
Rodrigo Orem <orem.rodrigo@gmail.com>
2020-06-19 19:58:16 -0300

    Corrige insert-pessoa.sql

--- sql/insert-pessoa.sql
+++ sql/insert-pessoa.sql
@@ -3,7 +3,7 @@ INSERT INTO pessoa (
   cpf,
   nome,
   endereco,
-  nascimento,
+  nascimento
 )
 VALUES
 
@@ -20,7 +20,6 @@ VALUES
   '62294303717',
   'Jaison Oliveira',
   'Rua das Flores, 23 - Cidade Jardim, Tocantins',
-  'UFPR',
   '1955-06-03T03:08:28.481Z'
 ),
 
@@ -29,8 +28,6 @@ VALUES
   '16537173353',
   'André Pereira',
   'Avenida Orem, 92 - República, Ceará',
-  'Oceanografia',
-  'UFRJ',
   '1975-04-03T09:42:28.392Z'
 ),
 
@@ -39,8 +36,6 @@ VALUES
   '21683645839',
   'Modesto Martins',
   'Praça Degenerada, 1 - Parque das Flores, São Paulo',
-  'Computação',
-  'UNESP',
   '1961-09-10T17:55:23.153Z'
 ),
 
@@ -49,8 +44,6 @@ VALUES
   '73843878250',
   'Ide Pereira',
   'Rua Descabida, 9912 - Morro do Totó, Amapá',
-  'Literatura Africana',
-  'UFRJ',
   '1954-11-25T03:00:50.324Z'
 ),
 
@@ -59,8 +52,6 @@ VALUES
   '02652119214',
   'Yngledhan Xavier',
   'Avenida Rupestre, 192 - Lapa, Rio Grande do Sul',
-  'Biologia Molecular',
-  'PUC',
   '1951-03-08T05:12:31.202Z'
 ),
 
@@ -69,8 +60,6 @@ VALUES
   '14653181357',
   'Charles Carvalho',
   'Praça Bem Querida, 62 - Parque da Nicotina, Mato Grosso',
-  'Literatura Africana',
-  'IME',
   '1987-05-13T13:02:00.843Z'
 ),
 
@@ -79,8 +68,6 @@ VALUES
   '42355198837',
   'Marcloves Silva',
   'Rua Realce Medieval, 91 - Butantã, Minas Gerais',
-  'Literatura Africana',
-  'ITA',
   '1951-05-03T08:38:31.595Z'
 ),
 
@@ -89,8 +76,6 @@ VALUES
   '53121214870',
   'Lucia Carvalho',
   'Avenida Jorges Bosques, 101 - Vila Weseslay, Distrito Federal',
-  'Psicanálise',
-  'INPA',
   '1950-10-18T04:17:00.559Z'
 ),
 
@@ -100,9 +85,7 @@ VALUES
   '80557886694',
   'Ruth Reis',
   'Rodovia Intergalática, 404 - Vila Medieval, Amazonas',
-  'Homeopatia',
-  'MACKENZIE',
-  '1971-02-22',
+  '1971-02-22'
 ),
 
 (
@@ -110,8 +93,6 @@ VALUES
   '78936128868',
   'Maraci Costa',
   'Rua Guerreira Importal, 504 - Parque Delgado, Santa Catarina',
-  'Direito Penal',
-  'PUC',
   '1974-01-13'
 ),
 
@@ -120,17 +101,14 @@ VALUES
   '27904634414',
   'Aderito Braga',
   'Avenida Realeza Fantástica, 500 - Hira, Paraná',
-  'Computação',
-  'IMPA',
   '1963-06-19'
+),
 
 (
   13,
   '92616832232',
   'Thelma Saraiva',
   'Rua Supresa Indevida, 200 - Repercussão, Alagoas',
-  'Geografia',
-  'UNICAMP',
   '1975-01-20'
 ),
 
@@ -139,8 +117,6 @@ VALUES
   '93754246348',
   'Romina Batista',
   'Avenida Abominável Jiló, 204 - Vila Fantástica, Rio Grande do Norte',
-  'Física Nuclear',
-  'IMPA',
   '1982-08-11'
 ),
 
@@ -149,10 +125,8 @@ VALUES
   '87677173225',
   'Ivaneide Reis',
   'Rua das Chaleiras, 418 - Parque da Glória, Piauí',
-  'Computação',
-  'UNICAMP',
   '1956-03-09'
-)
+),
 
 (
     16,
